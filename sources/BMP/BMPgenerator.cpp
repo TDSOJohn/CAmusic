8ca82b7dddcc86e3db9f59f01a944d298e734105
TDSOJohn <giovanni.basso@icloud.com>
2021-03-29 21:06:29 +0200

    added BMP generator to save content to bitmap

---
+++ sources/BMP/BMPgenerator.cpp
@@ -0,0 +1,93 @@
+#include "../../headers/BMP/BMPgenerator.hpp"
+
+#include <iostream>
+
+
+BMPgenerator::BMPgenerator():
+    size_x(100), size_y(100), mScaling(1), mBMP(size_x, size_y)
+{
+    srand(time(NULL));
+    generatePalettes();
+    choosen = rand()%3;
+}
+
+
+BMPgenerator::BMPgenerator(unsigned int x_in, unsigned int y_in, unsigned int scale_in):
+    size_x(x_in), size_y(y_in), mScaling(scale_in), mBMP(size_x, size_y)
+{
+    srand(time(NULL));
+    generatePalettes();
+    choosen = rand()%3;
+}
+
+
+void BMPgenerator::drawData(std::vector<int> const& data_in, unsigned int height)
+{
+    palette data_out;
+
+    for(int i = 0; i < data_in.size(); i++)
+        data_out.push_back(mPalettes[choosen][data_in[i]]);
+
+    drawLine(data_out, height);
+}
+
+
+void BMPgenerator::drawMatrix(std::vector<std::vector<int> > const& data_in)
+{
+
+}
+
+
+void BMPgenerator::saveImage(std::string const& filename)
+{
+    mBMP.save_image(filename);
+}
+
+
+void BMPgenerator::generatePalettes()
+{
+    mPalettes.push_back({{0, 0, 0}});
+    mPalettes.push_back({{0, 0, 0}});
+    mPalettes.push_back({{0, 0, 0}});
+
+    mPalettes[0]        ={{0, 0, 0},
+                        {223, 72, 92},
+                        {181, 125, 165},
+                        {133, 95, 46},
+                        {231, 173, 117},
+                        {130, 59, 41}}; // Autumn Picnic
+
+    mPalettes[1]        ={{0, 0, 0},
+                        {4, 17, 61},
+                        {21, 55, 86},
+                        {44, 101, 161},
+                        {137, 172, 189},
+                        {43, 99, 137}}; // Baby Blue
+
+    mPalettes[2]        ={{34, 5, 4},
+                        {160, 132, 51},
+                        {133, 97, 34},
+                        {82, 26, 18},
+                        {128, 25, 16}}; // Palermo Fish Market
+}
+
+
+void BMPgenerator::drawLine(std::vector<Pixel> const& data_in, unsigned int height)
+{
+    //Check if dimensions are at least enough for data writing
+    if((data_in.size() * mScaling) <= size_x)
+    {
+        for(int i = 0; i < data_in.size(); i++)
+        {
+            for(int x = 0; x < mScaling; x++)
+            {
+                for(int y = 0; y < mScaling; y++)
+                    mBMP.set_pixel( (i * mScaling + x),
+                                    (height * mScaling + y),
+                                    data_in[i].r,
+                                    data_in[i].g,
+                                    data_in[i].b);
+            }
+        }
+    }
+}
