b998a2ef16e9a204646e406c29fb1aa1d4aaf8a7
TDSOJohn <giovanni.basso@icloud.com>
2021-03-29 22:42:30 +0200

    improved Makefile. MIDIout is now broken, whatever dude

--- Makefile
+++ Makefile
@@ -1,14 +1,27 @@
-INCL = 	headers/CA/CA.hpp headers/CA/CA1d.hpp headers/CA/TotalisticCA.hpp \
-		headers/MIDI/MIDIout.hpp
+objects = main.o utilities.o ca.o ca1d.o totalisticca.o bmpgenerator.o
 
-OBJ	=	sources/CA/CA.cpp \
-		sources/CA/CA1d.cpp \
-		sources/CA/TotalisticCA.cpp \
-		sources/MIDI/MIDIout.cpp
+flags = --std=c++11 -c
+libs = -pthread -lrtmidi
 
-UTIL =	utilities.cpp utilities.hpp
+camusic	: $(objects)
+	g++ -o camusic $(objects)
 
-LIBS =	-lrtmidi -pthread
+main.o : main.cpp headers/CA/CA1d.hpp headers/CA/TotalisticCA.hpp headers/MIDI/MIDIout.hpp headers/BMP/BMPgenerator.hpp utilities.hpp
+	g++ $(flags) main.cpp
+utilities.o : utilities.cpp utilities.hpp
+	g++ $(flags) utilities.cpp
+ca.o : sources/CA/CA.cpp headers/CA/CA.hpp
+	g++ $(flags) sources/CA/CA.cpp
+ca1d.o : sources/CA/CA1d.cpp headers/CA/CA1d.hpp headers/CA/CA.hpp utilities.hpp
+	g++ $(flags) sources/CA/CA1d.cpp
+totalisticca.o : sources/CA/TotalisticCA.cpp headers/CA/TotalisticCA.hpp headers/CA/CA.hpp utilities.hpp
+	g++ $(flags) sources/CA/TotalisticCA.cpp
+#midiout.o : sources/MIDI/MIDIout.cpp headers/MIDI/MIDIout.hpp
+#	g++ $(flags) sources/MIDI/MIDIout.cpp $(libs)
+bmpgenerator.o : sources/BMP/BMPgenerator.cpp headers/BMP/BMPgenerator.hpp
+	g++ $(flags) sources/BMP/BMPgenerator.cpp
 
-a.out: $(INCL) $(OBJ) $(UTIL) main.cpp
-	g++ --std=c++11 $(OBJ) utilities.cpp main.cpp $(LIBS)
+
+.PHONY : clean
+clean :
+	rm camusic $(objects)
--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 #include <chrono>
 
 
-#include "headers/MIDI/MIDIout.hpp"
-
 #include "headers/CA/CA1d.hpp"
 #include "headers/CA/TotalisticCA.hpp"
+
+//#include "headers/MIDI/MIDIout.hpp"
+#include "headers/BMP/BMPgenerator.hpp"
 
 #include "utilities.hpp"
 
@@ -17,44 +18,59 @@ int main()
 {
     srand(time(NULL));
 
-    int states = 4;
-    int radius = 1;
-    int rule_size = pow(states, (radius * 2 + 1));
-    int size = 200;
-    int generations = 100;
+    int states              = 5;
+    int radius              = 1;
+    int rule1_size          = pow(states, (radius * 2 + 1));
+    int rule2_size          = states * (radius * 2 + 1);
+    int size                = 200;
+    int generations         = 150;
+
+    unsigned int scaling    = 15;
 
     std::vector<int> rule1;
     std::vector<int> rule2;
 
-    for(int i = 0; i < rule_size; i++)
-    {
+
+    for(int i = 0; i < rule1_size; i++)
         rule1.push_back(rand()%states);
-        std::cout << "\t" << rule1[i];
+
+    for(int i = 0; i < rule2_size; i++)
         rule2.push_back(rand()%states);
-    }
+
 
     rule1[0] = 0;
+    rule2[0] = 1;
 
-    CA1d ca1(rule1, radius, states);
-    TotalisticCA ca2(rule2, radius, states);
+    CA1d            ca1(rule1, radius, states);
+    TotalisticCA    ca2(rule2, radius, states);
 
-    ca1.initialize(size, CA1d::Start::Random);
+    ca1.initialize(size, CA1d::Start::Middle);
     ca2.initialize(size, TotalisticCA::Start::Middle);
 
+    BMPgenerator bmp(   size * scaling,
+                        generations * scaling,
+                        scaling);
 
-    ca1.print();
-    for(int i = 0; i < generations; i++)
+//    ca1.print();
+//    bmp.drawData(ca1.getData(), 0);
+
+/*    for(int i = 1; i < generations; i++)
     {
         ca1.generate();
-        ca1.print();
+//        ca1.print();
+        bmp.drawData(ca1.getData(), i);
     }
-
-    ca2.print();
-    for(int i = 0; i < generations; i++)
+*/
+//    ca2.print();
+    bmp.drawData(ca1.getData(), 0);
+    for(int i = 1; i < generations; i++)
     {
         ca2.generate();
-        ca2.print();
+//        ca2.print();
+        bmp.drawData(ca2.getData(), i);
     }
+
+    bmp.saveImage(ca2.str());
 
     std::cout << std::endl;
     return 0;
